6cfca4a693a9805601dfe6ee8ff8d981eb5a521a
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-12-01 13:46:19 +0300

    Updates

---
+++ precache-manifest.b82a8e5140eaeaf5cbe96eec178c469a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0213924ede6128e01a51e8642aa3f49",
+    "url": "/finlo/index.html"
+  },
+  {
+    "revision": "49519748ae7a7a6c32ac",
+    "url": "/finlo/static/css/main.3eaad394.chunk.css"
+  },
+  {
+    "revision": "112b371376b9bae9bcb0",
+    "url": "/finlo/static/js/2.2466caa5.chunk.js"
+  },
+  {
+    "revision": "49519748ae7a7a6c32ac",
+    "url": "/finlo/static/js/main.93ee7819.chunk.js"
+  },
+  {
+    "revision": "e68815c247e5395f39a4",
+    "url": "/finlo/static/js/runtime-main.efc714d6.js"
+  },
+  {
+    "revision": "f9b8d434a507ada13501acd333a88fa1",
+    "url": "/finlo/static/media/Logo-dark.f9b8d434.png"
+  },
+  {
+    "revision": "0e1249e9f26dc4f866fb86ff3c4bac6d",
+    "url": "/finlo/static/media/Logo.0e1249e9.png"
+  },
+  {
+    "revision": "546dc41a6a6ccc5910382f06c7e15b4b",
+    "url": "/finlo/static/media/bg-asset.546dc41a.png"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/finlo/static/media/hambuger-icon.86595150.svg"
+  },
+  {
+    "revision": "17405fa0e573d65195b989fc8b1ade02",
+    "url": "/finlo/static/media/man-sitting-in-front-of-keyboard-2422278.17405fa0.jpg"
+  },
+  {
+    "revision": "e5b73a28971bcb834eb072127f0e1bcc",
+    "url": "/finlo/static/media/section-leaves.e5b73a28.png"
+  },
+  {
+    "revision": "f2be9ebd25be0488106436afcb6a14e8",
+    "url": "/finlo/static/media/woman-wearing-beaded-white-necklace-1181623.f2be9ebd.jpg"
+  }
+]);
